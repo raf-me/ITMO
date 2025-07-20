78644e8c2efc09c2febfd85beb54244af6a48e9a
raf-me <rafael.sitdikov07@gmail.com>
2025-07-20 19:12:14 +0300

    Update Coordinates.java

--- ПИиКТ/Программирование/lab5/Код/Proga_Lab5-8/src/main/java/org/example/Coordinates/Coordinates.java
+++ ПИиКТ/Программирование/lab5/Код/Proga_Lab5-8/src/main/java/org/example/Coordinates/Coordinates.java
@@ -1,23 +1,35 @@
 package org.example.Coordinates;
 
+import java.util.Scanner;
+
 public class Coordinates {
     private Long x; //Поле не может быть null
     private Double y; //Максимальное значение поля: 95, Поле не может быть null
 
+    public Long getX() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите ваши координаты x: ");
+        x = sc.nextLong();
+        return x;
+    }
+
+    public Double getY() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите ваши координаты y: ");
+        y = sc.nextDouble();
+        return y;
+    }
+
     public Coordinates(Long x, Double y) {
+        getX();
+        getY();
+
         if (x == null || y == null || y > 95) {
             throw new IllegalArgumentException("Некорректные координаты!");
         }
 
         this.x = x;
         this.y = y;
-    }
-    public Long getX() {
-        return x;
-    }
-
-    public Double getY() {
-        return y;
     }
 
     @Override
