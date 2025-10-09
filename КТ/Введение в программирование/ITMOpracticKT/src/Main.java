ff6efdac3111c896f5381fc2eb552a6b1012d5d7
raf-me <rafael.sitdikov07@gmail.com>
2025-10-09 23:07:28 +0300

    Delete КТ/Введение в программирование directory

--- КТ/Введение в программирование/ITMOpracticKT/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    private static String answer;
-
-    public static void main(String[] args) {
-        Manager manager = new Manager();
-        manager.HomeWorkManager();
-    }
-}
