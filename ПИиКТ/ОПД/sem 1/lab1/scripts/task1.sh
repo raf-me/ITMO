1da340b71a6339e81896302634dc788affef2c99
raf-me <rafael.sitdikov07@gmail.com>
2025-10-17 17:40:45 +0300

    Новая директория ОПД

---
+++ ПИиКТ/ОПД/sem 1/lab1/scripts/task1.sh
@@ -0,0 +1,31 @@
+rmdir lab0
+mkdir lab0
+cd lab0
+echo "Способности Copycat Fake Tears Flail Low Lick Rock Throw
+Slam Faint Attack Rock Tomb Block Rock Slide Mimic Sucker Punch
+Double-Edge
+" > bonsly4
+mkdir claydol6
+cd claydol6
+mkdir pikachu
+mkdir dodrio
+mkdir spoink
+mkdir chinchou
+cd ..
+echo "Тип диеты Carnivore" > sandile7
+mkdir volcarona4
+cd volcarona4
+mkdir lumineon
+echo "Живёт
+Cave
+" > zubat
+mkdir hariyama
+mkdir shieldon
+cd ..
+mkdir wobbuffet7
+cd wobbuffet7
+mkdir munchlax
+mkdir dusclops
+mkdir conkeldurr
+cd ..
+echo "Живёт Freshwater Marsh" > wooper8
---
+++ ПИиКТ/ОПД/sem 1/lab1/scripts/task5.sh
@@ -0,0 +1,6 @@
+rm wooper8
+rm volcarona4/zubat
+rm volcarona4/zubatwoop
+rm volcarona4/zubatsandile
+rmdir wobbuffet7
+rm -r claydol6/chinchou
